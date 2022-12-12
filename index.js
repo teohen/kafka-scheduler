8431ebf8ef57e1034f0f34ee69235d4d6debcb1a
teteo <teodorohenriquecarvalho@gmail.com>
2022-12-11 22:39:19 -0300

    chore: adds a FIXME

--- index.js
+++ index.js
@@ -19,11 +19,5 @@ app.listen(PORT, () => {
 });
 
 
-// TODO: every time a timer is triggered, a message should be produced to the schedules topic AND, by consequence, 
-//        the storage will be updated with a empty schedule (cancelling any existing timer)
 
-
-// if a empty message comes and NO TIMER is present on the storage, cancel the timer and DO NOT SET the timer for that schedule
-
-
-// PS: its okay to clear and triggered timeout 
+// FIXME: rename the file missed-schedules-handler for something better
