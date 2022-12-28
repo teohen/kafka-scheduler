f7226920300cd3f60fa8c9da06c12f6d51c30d44
teteo <teodorohenriquecarvalho@gmail.com>
2022-12-27 22:06:38 -0300

    refactor: changes the emit event test of the event-manager module

--- test/event-manager.test.js
+++ test/event-manager.test.js
@@ -5,21 +5,21 @@ const assert = require('assert');
 const eventManager = require('../src/utils/event-manager')
 
 describe('Event Manager Suit', () => {
-
+  after(() => {
+    sinon.restore()
+  })
   afterEach(() => {
     sinon.restore()
   })
 
-  after(() => {
-    sinon.restore()
-  })
-
+  
   it('Should emit the SCHEDULES_STORED_EVENT event', () => {
+    const spy = sinon.spy(EventEmitter.prototype, "emit")
 
-    const spyConsole = sinon.spy(console, "log")
     eventManager.emitSchedulesStored()
 
-    assert.equal(spyConsole.calledWith('schedules stored!'), true)
+    const indexSchedulesStoredEvent = spy.getCalls().findIndex((call) => call.args[0] === eventManager.events.SCHEDULES_STORED_EVENT)
+    assert.equal(indexSchedulesStoredEvent != undefined, true)
   })
 
   it('Should assign the handler function to the event SCHEDULES_STORED_EVENT', () => {
